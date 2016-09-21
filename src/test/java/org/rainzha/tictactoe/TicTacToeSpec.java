2dd14f8aa340923e7536730ca3bef9df091fa8b6
rainzha <rainzha@163.com>
2016-09-21 15:29:51 +0800

    finished tic-tac-toe Requirement 1.

---
+++ src/test/java/org/rainzha/tictactoe/TicTacToeSpec.java
@@ -0,0 +1,44 @@
+package org.rainzha.tictactoe;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class TicTacToeSpec {
+    private TicTacToe ticTacToe;
+
+    @Before
+    public final void before() {
+        ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void whenXOutsideBoardThenRuntimeException() {
+        catchException(ticTacToe).play(5, 2);
+
+        assertTrue(caughtException() instanceof RuntimeException);
+        assertThat(caughtException().getMessage()).isEqualTo("X is outside board");
+    }
+
+    @Test
+    public void whenYOutsideBoardThenRuntimeException() {
+        catchException(ticTacToe).play(2, 5);
+
+        assertTrue(caughtException() instanceof RuntimeException);
+        assertThat(caughtException().getMessage()).isEqualTo("X is outside board");
+    }
+
+    @Test
+    public void whenOccupiedThenRuntimeException() {
+        ticTacToe.play(2, 1);
+        catchException(ticTacToe).play(2, 1);
+
+        assertTrue(caughtException() instanceof RuntimeException);
+        assertThat(caughtException().getMessage()).isEqualTo("Box is occupied");
+    }
+}
